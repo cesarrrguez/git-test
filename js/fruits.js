7138134e89d91044ec1f5de2d77abad92d3789ba
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-06 12:17:28 +0200

    Use of basic array methods. Closes #6 (+8 squashed commit)
    Squashed commits:
    
    [0cc3785] WIP: Array splices. Relates to #6
    
    [38b8fc1] WIP: Array add position. Relates to #6
    
    [bd83a60] WIP: Array delete position. Relates to #6
    
    [2c9cd00] WIP: Array unshift. Relates to #6
    
    [e3ef6e6] WIP: Array push. Relates to #6
    
    [aee1f94] WIP: Array shift. Relates to #6
    
    [9edc19c] WIP: Array pop. Relates to #6
    
    [662c464] WIP: Refactor Array prints out comment. Relates to #6

--- js/fruits.js
+++ js/fruits.js
@@ -1,2 +1,30 @@
 let fruits = ['Apple', 'Banana', 'Grapes'];
-console.log (fruits);   // Prints out "['Apple', 'Banana', 'Grapes']"
+console.log (fruits);                   // Prints out [ 'Apple', 'Banana', 'Grapes' ]
+
+fruits.pop()
+console.log(fruits);                    // Prints out [ 'Apple', 'Banana' ]
+
+fruits.shift();
+console.log(fruits);                    // Prints out [ 'Banana' ]
+
+fruits.push('Cherry');
+console.log(fruits);                    // Prints out [ 'Banana', 'Cherry' ]
+
+fruits.unshift('Lemon');
+console.log(fruits);                    // Prints out [ 'Lemon', 'Banana', 'Cherry' ]
+
+delete fruits[1];
+console.log(fruits);                    // Prints out [ 'Lemon', <1 empty item>, 'Cherry' ]
+
+fruits[1] = 'Orange';
+console.log(fruits);                    // Prints out [ 'Lemon', 'Orange', 'Cherry' ]
+
+fruits.splice(1,2);
+console.log(fruits);                    // Prints out [ 'Lemon' ]
+
+fruits.splice(2,0,'Orange', 'Cherry')
+console.log(fruits);                    // Prints out [ 'Lemon', 'Orange', 'Cherry' ]
+
+let citrusFruits = fruits.slice(0,2)
+console.log(fruits);                    // Prints out [ 'Lemon', 'Orange', 'Cherry' ]
+console.log(citrusFruits);              // Prints out [ 'Lemon', 'Orange' ]
