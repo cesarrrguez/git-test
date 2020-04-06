3c441e4c111f227b6174903bac036cac74071238
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-06 10:49:51 +0200

    Add else condition to age algorithm. Closes #4

--- js/age.js
+++ js/age.js
@@ -3,8 +3,11 @@ function checkAge(age) {
         return "You are not an adult yet!";
     } else if (age >= 18) {
         return "You are an adult!";
+    } else {
+        return "You don't have an age I guess!";
     }
 }
 
-console.log(checkAge(15));  // Prints out "You are not an adult yet!"
-console.log(checkAge(18));  // Prints out "You are an adult!"
+console.log(checkAge(15));      // Prints out "You are not an adult yet!"
+console.log(checkAge(18));      // Prints out "You are an adult!"
+console.log(checkAge("age"));   // Prints out "You don't have an age I guess!"
