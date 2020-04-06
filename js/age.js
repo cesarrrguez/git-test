eea929f6d49a9f7bbc539b30f7301876ca1e5f17
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-06 10:40:10 +0200

    WIP: Improvements on age algorithm. Relates #4

--- js/age.js
+++ js/age.js
@@ -1,7 +1,10 @@
 function checkAge(age) {
-    if (age >= 18) {
-        console.log("You are an adult!");
+    if (age < 18) {
+        return "You are not an adult yet!";
+    } else if (age >= 18) {
+        return "You are an adult!";
     }
 }
 
-checkAge(18);
+console.log(checkAge(15));  // Prints out "You are not an adult yet!"
+console.log(checkAge(18));  // Prints out "You are an adult!"
