752af082b56a7491f249e2e4d75b924d911a13be
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-11 19:03:58 +0200

    WIP: Create spring months. Relates to #11

--- js/months.js
+++ js/months.js
@@ -9,6 +9,18 @@ switch (month) {
         console.log('Month is February');
         break;
 
+    case 3:
+        console.log('Month is March');
+        break;
+
+    case 4:
+        console.log('Month is April');
+        break;
+
+    case 5:
+        console.log('Month is May');
+        break;
+
     case 12:
         console.log('Month is December');
         break;
