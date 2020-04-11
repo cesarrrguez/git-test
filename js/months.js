602edd5556cf10126b074e97c98bf1484b660c2d
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-11 19:38:45 +0200

    Create months algorithm (#13)
    * WIP: Create winter months. Relates to #11
    
    * WIP: Create spring months. Relates to #11
    
    * WIP: Create summer months. Relates to #11
    
    * WIP: Set selected month to 6. Relates to #11
    
    * Revert "WIP: Set selected month to 6. Relates to #11"
    
    This reverts commit 89eba75d9bf31f07382410f5d24969ab8d4174d4.
    
    * Create autumn months. Closes #11
    
    * BUGFIX: Set autumn months numbers. Fixes #12
    
    * Delete blank lines. Relates to #11

---
+++ js/months.js
@@ -0,0 +1,40 @@
+let month = 4;
+
+switch (month) {
+    case 1:
+        console.log('Month is January');
+        break;
+    case 2:
+        console.log('Month is February');
+        break;
+    case 3:
+        console.log('Month is March');
+        break;
+    case 4:
+        console.log('Month is April');
+        break;
+    case 5:
+        console.log('Month is May');
+        break;
+    case 6:
+        console.log('Month is June');
+        break;
+    case 7:
+        console.log('Month is July');
+        break;
+    case 8:
+        console.log('Month is August');
+        break;
+    case 9:
+        console.log('Month is September');
+        break;
+    case 10:
+        console.log('Month is October');
+        break;
+    case 11:
+        console.log('Month is November');
+        break;
+    case 12:
+        console.log('Month is December');
+        break;
+}
