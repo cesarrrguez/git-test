637a1925fc12670d832e40119415da5f502b179c
César Rodríguez <cesar.rrguez@gmail.com>
2020-04-11 19:01:49 +0200

    WIP: Create winter months. Relates to #11

---
+++ js/months.js
@@ -0,0 +1,15 @@
+let month = 4;
+
+switch (month) {
+    case 1:
+        console.log('Month is January');
+        break;
+
+    case 2:
+        console.log('Month is February');
+        break;
+
+    case 12:
+        console.log('Month is December');
+        break;
+}
